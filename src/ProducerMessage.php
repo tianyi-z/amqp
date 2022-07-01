7eeeb3a9f1e9edde6f722cab84babbf8feda895b
zhangtianyi <zhangtianyi@yuanxinjituan.com>
2022-07-01 18:34:46 +0800

    添加延时队列

--- src/ProducerMessage.php
+++ src/ProducerMessage.php
@@ -10,4 +10,16 @@ class ProducerMessage extends \Hyperf\Amqp\Message\ProducerMessage
         $this->properties['message_id'] = ApplicationContext::getContainer()->get(MessageId::class)->generateId();
         return parent::getProperties();
     }
+
+    /**
+     * 设置延迟时间
+     * @param int $millisecond 延迟多久,毫秒5秒就是5000
+     * @param string $name 阿里延时字段delay
+     * @return self
+     */
+    public function setDelayMs(int $millisecond, string $name = 'delay'): self
+    {
+        $this->properties['application_headers'] = new \PhpAmqpLib\Wire\AMQPTable([$name => $millisecond]);
+        return $this;
+    }
 }
